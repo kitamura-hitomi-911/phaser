b0437d629db111d91a71434bd6282ce08e70cc25
kitamura-hitomi-911 <hinitial@gmail.com>
2024-07-21 17:15:34 +0900

    とりあえず動く

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,194 @@
-import './style.css'
+import Phaser from "phaser";
+import "./style.css";
+/* 
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { setupCounter } from './counter.ts' */
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: "app",
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300, x: 0 },
+      debug: false,
+    },
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+new Phaser.Game(config);
+
+let player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+let stars: Phaser.Physics.Arcade.Group;
+let bombs: Phaser.Physics.Arcade.Group;
+let platforms: Phaser.Physics.Arcade.StaticGroup;
+let cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
+let gameOver: boolean = false;
+let score: number = 0;
+let scoreText: Phaser.GameObjects.Text;
+
+function preload(this: Phaser.Scene): void {
+  // アセットのロードなど
+  this.load.image("sky", "src/assets/sky.png");
+  this.load.image("ground", "src/assets/platform.png");
+  this.load.image("star", "src/assets/star.png");
+  this.load.image("bomb", "src/assets/bomb.png");
+  this.load.spritesheet("dude", "src/assets/dude.png", {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
+}
+
+function create(this: Phaser.Scene): void {
+  // ゲームオブジェクトの作成など
+
+  //  A simple background for our game
+  this.add.image(400, 300, "sky");
+
+  //  The platforms group contains the ground and the 2 ledges we can jump on
+  platforms = this.physics.add.staticGroup();
+
+  //  Here we create the ground.
+  //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+  platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+  //  Now let's create some ledges
+  platforms.create(600, 400, "ground");
+  platforms.create(50, 250, "ground");
+  platforms.create(750, 220, "ground");
+
+  // The player and its settings
+  player = this.physics.add.sprite(100, 450, "dude");
+
+  //  Player physics properties. Give the little guy a slight bounce.
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  // Our player animations, turning, walking left and walking right.
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  this.anims.create({
+    key: "turn",
+    frames: [{ key: "dude", frame: 4 }],
+    frameRate: 20,
+  });
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  //  Input Events
+  cursors = this.input?.keyboard?.createCursorKeys();
+
+  //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
+  stars = this.physics.add.group({
+    key: "star",
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 },
+  });
+
+  stars.children.iterate((child) => {
+    //  Give each star a slightly different bounce
+    (child as Phaser.Physics.Arcade.Sprite).setBounceY(
+      Phaser.Math.FloatBetween(0.4, 0.8)
+    );
+    return true;
+  });
+
+  bombs = this.physics.add.group();
+
+  // The score
+  scoreText = this.add.text(16, 16, "score: 0", {
+    fontSize: "32px",
+    color: "#000",
+  });
+
+  // Collide the player and the stars with the platforms
+  this.physics.add.collider(player, platforms);
+  this.physics.add.collider(stars, platforms);
+  this.physics.add.collider(bombs, platforms);
+
+  //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
+  this.physics.add.overlap(player, stars, collectStar, undefined, this);
+  this.physics.add.collider(player, bombs, hitBomb, undefined, this);
+}
+
+function update(this: Phaser.Scene) {
+  if (gameOver) {
+    return;
+  }
+
+  if (cursors?.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play("left", true);
+  } else if (cursors?.right.isDown) {
+    player.setVelocityX(160);
+    player.anims.play("right", true);
+  } else {
+    player.setVelocityX(0);
+    player.anims.play("turn");
+  }
+
+  if (cursors?.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
+}
+
+function collectStar(_player, star) {
+  star.disableBody(true, true);
+
+  //  Add and update the score
+  score += 10;
+  scoreText.setText("Score: " + score);
+
+  if (stars.countActive(true) === 0) {
+    stars.children.iterate((child) => {
+      (child as Phaser.Physics.Arcade.Sprite).enableBody(
+        true,
+        (child as Phaser.Physics.Arcade.Sprite).x,
+        0,
+        true,
+        true
+      );
+      return true;
+    });
+
+    const x =
+      player.x < 400
+        ? Phaser.Math.Between(400, 800)
+        : Phaser.Math.Between(0, 400);
+
+    const bomb = bombs.create(x, 16, "bomb");
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+  }
+}
+
+function hitBomb(_player, star) {
+  this.physics.pause();
+
+  player.setTint(0xff0000);
+
+  player.anims.play("turn");
+
+  gameOver = true;
+}
